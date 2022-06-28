b75d3f17af97aa063f25b5f99f505905f3005c07
tranthudat2k1 <thudatdl123@gmail.com>
2022-06-29 00:17:04 +0700

    setup cart context

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,9 +5,12 @@ import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
-const AddToCart = ({ id, stock, colors }) => {
+const AddToCart = ({ product}) => {
+  const {addToCart} = useCartContext();
+  const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
+  
   console.log(colors);
 
   const increase = () => {
@@ -61,7 +64,7 @@ const AddToCart = ({ id, stock, colors }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to='/cart' className="btn">add to cart</Link>
+        <Link to='/cart' className="btn" onClick={()=>addToCart(id,mainColor,amount,product)}>add to cart</Link>
       </div>
     </Wrapper>
   );
