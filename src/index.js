211d76011fc7cc85206ab1281272066f6ff12f05
tranthudat2k1 <thudatdl123@gmail.com>
2022-07-01 22:43:10 +0700

    Auth0 finsh

--- src/index.js
+++ src/index.js
@@ -7,16 +7,25 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+// dev-ljvyyvdn.us.auth0.com
+// k9MXaJgTKwtYbzxMVX3KJJ9624egWxWS
 ReactDOM.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider domain="dev-ljvyyvdn.us.auth0.com"
+      clientId="k9MXaJgTKwtYbzxMVX3KJJ9624egWxWS"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <UserProvider>
+        <ProductsProvider>
+          <FilterProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </FilterProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
